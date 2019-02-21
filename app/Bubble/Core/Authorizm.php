89c29373e2eb49f9401f31e8cf445e69ee639af0
Moviet <snorthfed@gmail.com>
2019-02-21 20:02:17 +0700

    Just for nice readable

--- app/Bubble/Core/Authorizm.php
+++ app/Bubble/Core/Authorizm.php
@@ -6,27 +6,27 @@ use App\Bubble\Core\Identicated;
 
 trait Authorizm
 {
-		use Identicated;
-		
-		public function admin()
-		{
-				return intval(base64_decode(session($this->tokenId))); 
-		}
+    use Identicated;
 
-		public function isUser()
-		{
-				return (session()->has($this->tokenId)) ? 'active' : 'inactive';
-		}
+    public function admin()
+    {
+        return intval(base64_decode(session($this->tokenId))); 
+    }
 
-		public function backToLogin()
-		{
-				return session()->flush();
-		}
+    public function isUser()
+    {
+        return (session()->has($this->tokenId)) ? 'active' : 'inactive';
+    }
 
-		public function getToken()
-		{
-				$token = session()->flash(
-						$this->token, $this->uin()
-				);
-		}
+    public function backToLogin()
+    {
+        return session()->flush();
+    }
+
+    public function getToken()
+    {
+        $token = session()->flash(
+            $this->token, $this->uin()
+        );
+    }
 }
