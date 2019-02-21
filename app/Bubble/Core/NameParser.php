26d0901e6373fc2856e44a003254d667b963be52
Moviet <snorthfed@gmail.com>
2019-02-21 20:04:02 +0700

    Just for nice reading

--- app/Bubble/Core/NameParser.php
+++ app/Bubble/Core/NameParser.php
@@ -7,28 +7,27 @@ namespace App\Bubble\Core;
  */
 class NameParser
 {
-		public function cut($string = null) 
-		{
-				$attributes = explode(' ', $string);		
-				return (empty($attributes[1])) ? ucfirst($attributes[0]) : ucfirst($attributes[0]);
-		}
+    public function cut($string = null) 
+    {
+        $attributes = explode(' ', $string);		
+        return (empty($attributes[1])) ? ucfirst($attributes[0]) : ucfirst($attributes[0]);
+    }
 
-		public function fully($string = null) 
-		{
-				$attributes = explode(' ', $string);		
-				return (empty($attributes[1])) ? ucfirst($attributes[0]) : 
-								ucfirst($attributes[0]) . ' ' . ucfirst($attributes[1]);
-		}
+    public function fully($string = null) 
+    {
+        $attributes = explode(' ', $string);		
+        return (empty($attributes[1])) ? ucfirst($attributes[0]) : ucfirst($attributes[0]) . ' ' . ucfirst($attributes[1]);
+    }
 
-		public function cutNormal($string = null) 
-		{
-				$attributes = explode(' ', $string);		
-				return (empty($attributes[1])) ? ($attributes[0]) : ($attributes[0]);
-		}
+    public function cutNormal($string = null) 
+    {
+        $attributes = explode(' ', $string);		
+        return (empty($attributes[1])) ? ($attributes[0]) : ($attributes[0]);
+    }
 
-		public function cutSmall($string = null) 
-		{
-				$attributes = explode(' ', $string);		
-				return (empty($attributes[1])) ? strtolower($attributes[0]) : strtolower($attributes[0]);
-		}
+    public function cutSmall($string = null) 
+    {
+        $attributes = explode(' ', $string);		
+        return (empty($attributes[1])) ? strtolower($attributes[0]) : strtolower($attributes[0]);
+    }
 }
