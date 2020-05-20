9b3ee6a4ab460db1ec85f7c8c3d349bff1a4e9c8
moviet <snorthfed@gmail.com>
2020-05-21 03:47:37 +0700

    Ignore download vendor folder

--- vendor/moviet/php-encryption/src/Exceptions/DecryptException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Moviet\Heavy\Exceptions;
-
-use \Exception;
-
-class DecryptException extends Exception
-{
-}
--- vendor/moviet/php-encryption/src/Exceptions/EqualsException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Moviet\Heavy\Exceptions;
-
-use \Exception;
-
-class EqualsException extends Exception
-{
-}
